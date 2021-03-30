08c94b49bbee6f7b9f7cd8c0f9dee8e18682e3f5
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-30 19:11:24 +0000

    created employees for exercise 6

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -12,13 +12,15 @@ puts "----------"
 # bundle exec ruby exercises/exercises_1.rb
 
 class Store < ActiveRecord::Base
-  end
+  has_many :employees
+end
 
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
 
 store1 = Store.create(name: "Burnaby", annual_revenue: 224000, mens_apparel: true, womens_apparel: true)
 store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
-
-
 
 puts Store.count
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,14 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+#put employee class inside exercise 1
+
+@store1.employees.create(first_name: "Polkadot", last_name: "Patterson", hourly_rate: 100)
+@store1.employees.create(first_name: "Ziwa", last_name: "Mueller", hourly_rate: 45)
+@store1.employees.create(first_name: "Commissioner", last_name: "Vapor", hourly_rate: 69)
+@store1.employees.create(first_name: "Fish", last_name: "Summer", hourly_rate: 24)
+
+@store2.employees.create(first_name: "Eugenia", last_name: "Garbage", hourly_rate: 35)
+@store2.employees.create(first_name: "Jesus", last_name: "Koch", hourly_rate: 30)
+@store2.employees.create(first_name: "Lott", last_name: "Greer", hourly_rate: 95)
+@store2.employees.create(first_name: "Ortiz", last_name: "Morse", hourly_rate: 12)
