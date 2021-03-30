70936b3e76710a3313c1ef897a2fc046b8470916
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-30 18:23:52 +0000

    completed first 2 exercises

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,21 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+# git clone ar-exercises.git ar-exercises
+# cd ar-exercises
+# psql postgres
+# CREATE DATABASE ar_exercises;
+# bundle install
+# bundle exec ruby exercises/exercises_1.rb
+
+class Store < ActiveRecord::Base
+  end
+
+
+store1 = Store.create(name: "Burnaby", annual_revenue: 224000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+
+
+puts Store.count
